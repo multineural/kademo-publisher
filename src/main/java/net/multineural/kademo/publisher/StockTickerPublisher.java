255a86505b340c1da7132c8151197bcd6a3f4f52
multineural <jim@livingblast.com>
2020-12-13 16:37:19 -0700

    use more robust message structure with key

--- src/main/java/net/multineural/kademo/publisher/StockTickerPublisher.java
+++ src/main/java/net/multineural/kademo/publisher/StockTickerPublisher.java
@@ -1,12 +1,12 @@
 package net.multineural.kademo.publisher;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.Random;
@@ -32,7 +32,9 @@ public class StockTickerPublisher {
         final int price = random.nextInt(100) + 200;
         final String message = "{\"" + tickerTopic + "\":" + price + "}";
 
-        kafkaTemplate.send(tickerTopic, message)
+        ProducerRecord<String, String> record = generateRecord(message);
+
+        kafkaTemplate.send(record)
             .addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
                 @Override
                 public void onFailure(Throwable throwable) {
@@ -40,11 +42,19 @@ public class StockTickerPublisher {
                 }
 
                 @Override
-                public void onSuccess(SendResult<String, String> stringStringSendResult) {
-                    logger.info("Sent " + message);
+                public void onSuccess(SendResult<String, String> sendResult) {
+                    logger.info("Sent " + record.toString());
                 }
             });
 
+    }
+
+
+    private ProducerRecord<String, String> generateRecord(final String message) {
+
+        final String key = tickerTopic + "-" + System.currentTimeMillis();
+        ProducerRecord<String, String> record = new ProducerRecord<>(tickerTopic, key, message);
+        return record;
     }
 
 
