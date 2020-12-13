b26de546a80a8c9717802c8904918f960c4626bc
multineural <jim@livingblast.com>
2020-12-12 19:26:15 -0700

    basic publisher stuff

--- src/main/java/net/multineural/kademo/publisher/StockTickerPublisher.java
+++ src/main/java/net/multineural/kademo/publisher/StockTickerPublisher.java
@@ -1,23 +1,51 @@
 package net.multineural.kademo.publisher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.Random;
 
 @Service
 public class StockTickerPublisher {
 
-    public static boolean OPEN = false;
+    Logger logger = LoggerFactory.getLogger(StockTickerPublisher.class);
 
-    public void openMarket() {
-        OPEN = true;
+    private final String tickerTopic = "DIA_TEST";
+    private final Random random = new Random();
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public StockTickerPublisher(final KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void closeMarket() {
-        OPEN = false;
+    @Scheduled(fixedRate = 1000)
+    public void updateTickers() {
+
+        // this is our one simulated ticker symbol: DIA_TEST
+        final int price = random.nextInt(100) + 200;
+        final String message = "{\"" + tickerTopic + "\":" + price + "}";
+
+        kafkaTemplate.send(tickerTopic, message)
+            .addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+                @Override
+                public void onFailure(Throwable throwable) {
+                    logger.info("FAILED");
+                }
+
+                @Override
+                public void onSuccess(SendResult<String, String> stringStringSendResult) {
+                    logger.info("Sent " + message);
+                }
+            });
+
     }
 
-    public void sendTicker() {
-        while(OPEN) {
 
-        }
-    }
 }
